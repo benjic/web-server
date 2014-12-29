43f254ee23c9ae4e61cce09e3b5d14f0660fa004
Benjamin Campbell <benjamin.campbell@umconnect.umt.edu>
2014-12-29 12:28:33 -0700

    Bind port to socket

--- webserver.c
+++ webserver.c
@@ -1,4 +1,5 @@
-#include <stdlib.h>
+#include <netinet/in.h>
+#include <stdio.h>
 #include <sys/socket.h>
 
 int main(int argc, char** argv)
@@ -6,7 +7,21 @@ int main(int argc, char** argv)
 
 	int listen_fd;
 
+	struct sockaddr_in serv_addr;
+
 	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(8080);
+
+	bind(listen_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	
+	if (listen(listen_fd, 64)) {
+		perror("listen");
+	} else {
+		
+	}
 
 	return 0;
 }
